e9b734666b04e680070523bf0be84243daec3518
Simone <simone.garbo99@gmail.com>
2021-01-21 21:29:03 +0100

    Added spinner, added fonts, created new branch ACAB

--- main.js
+++ main.js
@@ -2,22 +2,33 @@ let allItems;
 let loaded = false;
 
 function updateItemDisplay(itemsToDisplay) {
-  for (const item of itemsToDisplay) {
-    $("#items-display").append(`<div>${item["name"]}</div>`);
-  }
+    for (const item of itemsToDisplay) {
+        $("#items-display").append(`<div>${item["name"]}</div>`);
+    }
 }
 
 function finishedLoading() {
-  loaded = true;
-  updateItemDisplay(allItems);
+    loaded = true;
+    isLoaded();
+    updateItemDisplay(allItems);
 }
 
 function requestItems() {
-  $.ajax({
-    url: "https://api.wynncraft.com/public_api.php?action=itemDB&category=all",
-    success: (data) => {
-      allItems = data["items"];
-      finishedLoading();
+    $.ajax({
+        url: "https://api.wynncraft.com/public_api.php?action=itemDB&category=all",
+        success: (data) => {
+            allItems = data["items"];
+            finishedLoading();
+        }
+    });
+    isLoaded();
+}
+
+function isLoaded(){
+    let itemsDisplay = document.querySelector('#items-display');
+    if(loaded !== true){
+        itemsDisplay.classList.add('loading');
+    } else {
+        itemsDisplay.classList.remove('loading');
     }
-  })
 }
