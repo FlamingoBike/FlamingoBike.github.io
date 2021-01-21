5b1072af732d33b58f3eb2f0fb56c332fb4c7e56
FlamingoBike <36803440+FlamingoBike@users.noreply.github.com>
2021-01-21 19:49:23 +0100

    added item requesting and adding

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const itemsToDisplay = {};
+let allItems;
+let loaded = false;
 
-function updateItemDisplay() {
+function updateItemDisplay(itemsToDisplay) {
   for (const item of itemsToDisplay) {
-    $("#items-display").add("div")
+    $("#items-display").append(`<div>${item["name"]}</div>`);
   }
+}
+
+function finishedLoading() {
+  loaded = true;
+  updateItemDisplay(allItems);
+}
+
+function requestItems() {
+  $.ajax({
+    url: "https://api.wynncraft.com/public_api.php?action=itemDB&category=all",
+    success: (data) => {
+      allItems = data["items"];
+      finishedLoading();
+    }
+  })
 }
